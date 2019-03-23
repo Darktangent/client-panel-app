d57b16cdb1714d284b0a6566503032e04283358c
Ron Ganguly <c1ph3r9@gmail.com>
2019-03-23 13:05:34 -0500

    add context and implement remove state

--- contactmanager/src/components/Contact.js
+++ contactmanager/src/components/Contact.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Consumer } from '../context';
 
 class Contact extends Component {
 	state = {
@@ -10,26 +11,43 @@ class Contact extends Component {
 			showContactInfo: !this.state.showContactInfo
 		});
 	};
+	onDeleteClick = (id, dispatch) => {
+		// this.props.deleteClickHandler();
+		dispatch({ type: 'DELETE_CONTACT', payload: id });
+	};
 	render() {
 		const { contact } = this.props;
 		const { showContactInfo } = this.state;
 		return (
-			<div className='card card-body mb-3'>
-				<h4>
-					{contact.name}{' '}
-					<i
-						onClick={this.onShowClick}
-						// .bind(this, contact.name)
-						className='fas fa-sort-down'
-					/>
-				</h4>{' '}
-				{showContactInfo ? (
-					<ul className='list-group'>
-						<li className='list-group-item'>{contact.email}</li>
-						<li className='list-group-item'>{contact.phone}</li>
-					</ul>
-				) : null}
-			</div>
+			<Consumer>
+				{value => {
+					const { dispatch } = value;
+					return (
+						<div className='card card-body mb-3'>
+							<h4>
+								{contact.name}{' '}
+								<i
+									onClick={this.onShowClick}
+									// .bind(this, contact.name)
+									className='fas fa-sort-down'
+									style={{ cursor: 'pointer' }}
+								/>
+								<i
+									className='fas fa-times'
+									style={{ cursor: 'pointer', float: 'right', color: 'red' }}
+									onClick={this.onDeleteClick.bind(this, contact.id, dispatch)}
+								/>
+							</h4>{' '}
+							{showContactInfo ? (
+								<ul className='list-group'>
+									<li className='list-group-item'>{contact.email}</li>
+									<li className='list-group-item'>{contact.phone}</li>
+								</ul>
+							) : null}
+						</div>
+					);
+				}}
+			</Consumer>
 		);
 	}
 }
@@ -38,5 +56,6 @@ Contact.propTypes = {
 	// email: PropTypes.string.isRequired,
 	// phone: PropTypes.string.isRequired
 	contact: PropTypes.object.isRequired
+	// deleteClickHandler: PropTypes.func.isRequired
 };
 export default Contact;
